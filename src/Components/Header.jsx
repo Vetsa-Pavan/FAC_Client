8056a461c23d323c3e35a15e15e03ff70af31c29
Vetsa-Pavan <129641142+Vetsa-Pavan@users.noreply.github.com>
2024-03-22 16:40:02 +0530

    Mobile_Header
    Mobile_Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import { useState , useEffect} from 'react';
 import Image from "next/image";
 import Logo from '../../public/Images/navLogo.png';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCaretDown} from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown, faEnvelope, faPhone, faMapLocationDot} from "@fortawesome/free-solid-svg-icons";
 import { faInstagram, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons"
 
 
@@ -18,14 +18,35 @@ export default function Header() {
   useEffect(() => {
     window.addEventListener(
       "resize",
-      () => window.innerWidth >= 960 && setIsListOpen(false),
+      () => window.innerWidth >= 1023 && setIsListOpen(false),
     );
   }, []);
 
   return (
-    <header className=' bg-[#dfe6e9] lg:justify-between'>
+    <header className='  lg:justify-between'>   
+      <div className=' bg-[#a8b6c2] flex flex-row lg:hidden h-14 gap-2 items-center justify-between'> 
+          <div className='flex flex-row gap-3 items-center px-4 font-bold'>
+            <FontAwesomeIcon icon={faEnvelope} size="lg" className="text-white"/>        
+            <p>foracausehitam@gmail.com</p>  
+            <FontAwesomeIcon icon={faPhone} size="lg" className="text-white"/>
+            <p>+91 6301136672</p>       
+          </div>
+          <div className="flex flex-row gap-2 items-center ">
+            <span className="w-10 h-10 flex items-center justify-center rounded-full bg-gradient-to-tr from-pink-500 to-red-500">      
+            <a href='https://www.instagram.com/foracause__/'><FontAwesomeIcon icon={faInstagram} size="lg" className="text-white"/></a>            
+           </span> 
+
+            <span className="  w-10 h-10 flex items-center justify-center rounded-full bg-blue-500">
+             <a href='https://www.linkedin.com/in/for-a-cause-social-welfare-association-6a2899220'><FontAwesomeIcon icon={faLinkedin} size="lg" className="text-white"/></a>
+            </span>
+
+            <span className=" w-10 h-10 flex items-center justify-center rounded-full bg-blue-400">
+             <a href='https://x.com/foracausehitam?t=yUmnRxgcf7DnLt-e2UK88w&s=09'><FontAwesomeIcon icon={faTwitter} size="lg" className="text-white"/></a>
+           </span>
+          </div>            
+      </div>
       <nav className="flex items-center justify-between px-4 lg:pl-16 py-4 lg:flex lg:flex-row ">
-      <div className='flex flex-col items-center '>
+        <div className='flex flex-col items-center'>
           <a href="/">
               <Image src={Logo} className="w-32" />
           </a>
@@ -33,8 +54,8 @@ export default function Header() {
           <p className='font-bold text-sm text-nowrap'> Social Welfare Association</p>
         </div>
           
-        <div className={`absolute -top-96 left-0 w-full bg-[#dfe6e9]  flex flex-col lg:static lg:flex-row  lg:justify-center lg:gap-5  ${isListOpen && 'top-32'} `}>    
-          <ul className="flex flex-col justify-between lg:flex-row lg:items-center left-5 lg:gap-1 ">
+        <div className={`absolute -top-96 left-0 w-full   flex flex-col lg:static lg:flex-row  lg:justify-center lg:gap-5  ${isListOpen && 'top-44'} `}>    
+          <ul className="flex flex-col justify-between lg:flex-row lg:items-center left-5 lg:gap-1">
             <li className="border-solid border-y-2 border-[#C4C4C4] py-3 px-2 flex justify-between lg:justify-normal lg:flex lg:text-nowrap lg:gap-2 lg:border-none">
               <div>
                 <a href='/' className=' font-bold text-xs text-[#2e2828]'>WHO WE ARE</a>
@@ -85,19 +106,19 @@ export default function Header() {
             <button className="bg-[#40739e] hover:bg-[#619ecf] text-white font-bold py-2 px-4 rounded w-full lg:text-nowrap">LOG IN</button>
             <button className="hidden lg:inline-block lg:bg-[#40739e] hover:bg-[#619ecf] font-bold rounded text-white py-2 px-4 lg:text-nowrap">DONATE</button>
           </div>
-        </div>
+         </div>
 
-        <div className="lg:flex lg:flex-row lg:gap-1 items-center" > 
+         <div className="lg:flex lg:flex-row lg:gap-2 items-center" >
           <span className=" hidden w-10 h-10 lg:flex items-center justify-center rounded-full bg-gradient-to-tr from-pink-500 to-red-500">      
-            <a href='/'><FontAwesomeIcon icon={faInstagram} size="lg" className="text-white"/></a>            
+            <a href='https://www.instagram.com/foracause__/'><FontAwesomeIcon icon={faInstagram} size="lg" className="text-white"/></a>            
           </span> 
 
           <span className=" hidden w-10 h-10 lg:flex items-center justify-center rounded-full bg-blue-500">
-          <a href='/'><FontAwesomeIcon icon={faLinkedin} size="lg" className="text-white"/></a>
+          <a href='https://www.linkedin.com/in/for-a-cause-social-welfare-association-6a2899220'><FontAwesomeIcon icon={faLinkedin} size="lg" className="text-white"/></a>
           </span>
 
           <span className="hidden w-10 h-10 lg:flex items-center justify-center rounded-full bg-blue-400">
-          <a href='/'><FontAwesomeIcon icon={faTwitter} size="lg" className="text-white"/></a>
+          <a href='https://x.com/foracausehitam?t=yUmnRxgcf7DnLt-e2UK88w&s=09'><FontAwesomeIcon icon={faTwitter} size="lg" className="text-white"/></a>
           </span>             
          </div>
 
